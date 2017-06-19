65611904531b43d3c0798d0be5e35025e78369aa
Stephanie Wissel <swissel@calpoly.edu>
2017-06-19 08:52:30 -0700

    committing e-field acceptance calc.

--- 03_Detector/tau_Shower_Efield_Sim_lib.py
+++ 03_Detector/tau_Shower_Efield_Sim_lib.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import os
 tau_exit_num_events = 1.e6
 
 # Define constants
@@ -8,45 +8,70 @@ tau_life_time  = 2.906e-13  # seconds
 tau_mass       = 1776.82e6  # eV/c2
 speed_of_light = 299792.458 # km / second
 
-# Threshold voltage based on temperature:
+# Threshold voltage based on galactic noise and system temperature:
 T_ant  = 105.     # Kelvin (based on elog 655 entry by P. Gorham)
 T_sys  = 140.     # System temperature at the lower part of the ANITA band from elog 119
 kB     = 1.38e-23 # in Volts^2/Ohm/Hz/Kelvin
 Z_load = 50.      # 50 Ohm system
-BW     = 400.e6   # bandwidth for first two ANITA bands.
+#BW     = 400.e6   # bandwidth for first two ANITA bands.
 #threshold_voltage = 8.9e-06 # V, this is 8.9 microVolts for sky temp 290 Kelvin T_sys
 #threshold_voltage =np.sqrt(kB*(T_ant+T_sys)*Z_load*BW) # V, this is 8.22 microVolts for the parameters given above
 
-threshold_voltage = 72.e-6 # V, this is based on taking half the peak field for the weakest ANITA-1 EAS candidate. (0.466 mV/m convolved with antenna effective height at 300 MHz). 
+#threshold_voltage = 72.e-6 # V, this is based on taking half the peak field for the weakest ANITA-1 EAS candidate. (0.466 mV/m convolved with antenna effective height at 300 MHz). 
+threshold_voltage_snr = 5.0
+print 'threshold_voltage_snr', threshold_voltage_snr
+####################################################################################
 
-print 'threshold_voltage', threshold_voltage
+def galactic_temperature(f_MHz):
+    # Dulk 2001
+    nu = f_MHz # Hz
+    tau = 5.0 * pow(nu, -2.1)
+    Ieg = 1.0610e-20
+    Ig  = 2.48e-20
+    # Iv in  W/m^2/Hz/sr
+    Iv = Ig * pow(nu, -0.52) * (1-np.exp(-tau))/tau + Ieg * pow(nu, -0.80) * np.exp(-tau)
+    
+    kB = 1.38064852e-23 # Watts / Hz / K
+    c = 299792458 # m/s
+    temp = Iv * c**2 / (2*(nu*1e6)**2)/kB
+    ## IV is the intensity
+    ## temp is the galactic noise temperature in Kelvin
+    return Iv, temp # W/m^2/Hz/sr, K
+
+####################################################################################
+
+def noise_v_sq(freq_MHz, Z_L, Z_A):
+    val = kB_W_Hz_K*galactic_temperature(freq_MHz)[1]*Z_L
+    # This is in Volts^2/Hz
+    return val 
+
 ####################################################################################
 
 def tau_lepton_decay_range(tau_lepton_energy):
     return speed_of_light * 10**(tau_lepton_energy)  / tau_mass * tau_life_time
 
 ####################################################################################
+def load_efield_interpolator(h, EFIELD_LUT_file_name=os.environ['TAU_ACC_ZHAIRES_DIR'] + '/interpolator_efields_%dkm.npz'):
+    # the interpolator is for 10-MHz subbands and 
+    # is called as interpolator(zenith_angle, starting_frequency,psi_angle)
+    # zenith angle is the shower zenith angle in deg.
+    # staring_frequency is the lowest frequency in the band in MHz
+    # psi_angle is the angel off the shower axis, 
+    # equivalent to view angle in deg.
+    interp_file = np.load(EFIELD_LUT_file_name)
+    return interp_file['efield_interpolator_list'][()]
+####################################################################################
 
-def E_field_model(view_angle_deg, log10_tau_energy, distance_km):
-    # Lorentzian beam pattern based on Harm's results
+def E_field_interp(view_angle_deg, zenith_angle_deg, f_Lo, f_High, log10_tau_energy, distance_km):
+    # Lorentzian beam pattern based on 10-MHz filtered subbands of Harm's results
     # Returns electric field peak in V/m
-    
-    # This is the older Lorentz fit to a lateral distribution function that was actually at 75 degree zenith exit angle rather than 65.
-    #E_0 =  1.2e-4*10**(log10_tau_energy - 17.) # overall amplitude in ANITA 200 - 1200 MHz band
-    #E_0 *= 85.2/distance_km # corrected for the distance the shower was simulated at.
-    #arg =  (view_angle_deg - 1.1)/0.14 
-    #return E_0 * 1. / (1+arg**2)
-
-    # This is a more detailed fit to the lateral distribution function at 65 for a 200 - 1200 MHz band.
-    E_0  = 0.183*1.e-3     # V/m at ground level for 10^17 eV tau lepton.
-    peak_view_angle = 1.01 # degrees
-    width = 0.160          # degrees
-    E_1 = 0.00385*1.e-3    # V/m at ground level for 10^17 eV tau lepton.
-    width2 = 1.14          # degrees
-    v = (view_angle_deg-peak_view_angle)/width 
-    frac_gauss = 0.825 # fraction of Gaussian peak ( 1-frac_gauss is lorentzian)
-    # The E_field model is a linear combination of a Gaussian and a Lorentzian centered at the peak view angle. A Gaussian centered at 0 deg is added to account for the central lobe. 
-    E_field = E_0*(frac_gauss*np.exp(-v**2/2.) + (1-frac_gauss)/(1+v**2)) + E_1*np.exp(-view_angle_deg**2/2./width2**2)
+  
+    # Since the efields are stored in 10-MHz subbands
+    # integrate over the range from f_Lo to f_High in 10-MHz bands
+    E_field = 0.
+    for i_f_Lo, freq in enumerate(np.arange(f_Lo, f_High + 10., 10.)):
+    	E_field += efield_interpolator_list[i_f_Lo](zenith_angle_deg, view_angle_deg)   
+    	#E_field += 1
     E_field *= 86.4/distance_km   # distance to tau decay point correction
     E_field *= 10**(log10_tau_energy - 17.) # Energy scaling
     return E_field
@@ -124,6 +149,27 @@ def get_view_angle(k_x, k_y, k_z, x_pos, y_pos, z_pos, x_det, y_det, z_det):
 
 ####################################################################################
 
+def get_distance_to_detector(x_pos, y_pos, z_pos, x_det, y_det, z_det):
+    x_pd = x_pos - x_det
+    y_pd = y_pos - y_det
+    z_pd = z_pos - z_det
+    return np.sqrt( x_pd**2 + y_pd**2 + (z_pd - Earth_radius)**2 )
+
+####################################################################################
+
+def decay_point_geom(k_x, k_y, k_z, x_exit, y_exit, z_exit, X0_decay, x_det, y_det, z_det, exit_view_angle):
+    exit_phi_angle = np.arctan(k_y/k_x);
+    z_decay = X0_decay * np.sin(exit_view_angle) + z_exit
+    y_decay = (X0_decay * np.cos(exit_view_angle))*np.sin(exit_phi_angle) + y_exit
+    x_decay = (X0_decay * np.cos(exit_view_angle))*np.cos(exit_phi_angle) + x_exit
+    
+    decay_view_angle = get_view_angle(k_x, k_y, k_z, x_decay, y_decay, z_decay, x_det, y_det, z_det)
+    decay_dist_to_detector = get_distance_to_detector(x_decay, y_decay, z_decay, x_det, y_det, z_det )
+    
+    return x_decay, y_decay, z_decay, decay_view_angle, decay_dist_to_detector
+
+####################################################################################
+
 def GEOM(altitude, num_events, view_angle_cut_deg = 5.):
     ##############################################
     # STEPS
@@ -178,7 +224,7 @@ def parse_input_args(input_arg_string):
 
 ####################################################################################
 
-def A_OMEGA_tau_exit(geom_file_name, LUT_file_name, cut_ang, outTag='test'):
+def A_OMEGA_tau_exit(geom_file_name, LUT_file_name, EFIELD_LUT_file_name, cut_ang, f_Lo, f_High, outTag='test'):
 
     # 1. Load geometry file
     GEOM = np.load(geom_file_name)
@@ -189,13 +235,21 @@ def A_OMEGA_tau_exit(geom_file_name, LUT_file_name, cut_ang, outTag='test'):
     cos_theta_exit  = GEOM['cos_theta_exit']
     exit_view_angle = GEOM['exit_view_angle']
     x_exit          = GEOM['x_exit']
+    y_exit          = np.zeros(len(x_exit))
+    #y_exit          = GEOM['y_exit']
     z_exit          = GEOM['z_exit']
+    k_x             = GEOM['k_x']
+    k_y             = GEOM['k_y']
+    k_z             = GEOM['k_z']
     A_geom          = GEOM['A_geom']
     altitude = float(GEOM_inputs['altitude'])
     GEOM.close()
     N_tot = len(cos_theta_exit)
     print 'Number of Geometry Events', N_tot
     print ''
+    x_det = 0.
+    y_det = 0.
+    z_det = altitude
 
     # 2. Impose a viewing angle cut
     view_cut = exit_view_angle*180./np.pi<cut_ang
@@ -203,38 +257,56 @@ def A_OMEGA_tau_exit(geom_file_name, LUT_file_name, cut_ang, outTag='test'):
     A_Omega = A_geom* float(N_cut) / float(N_tot)
     # TODO: fix to calculate dist_to_detector from decay point rather than from
     # the exit point which is what it is now.
-    dist_to_detector = np.sqrt( x_exit**2 + (z_exit - altitude - Earth_radius)**2 ) [view_cut]
+    dist_exit_to_detector = get_distance_to_detector(x_exit, y_exit, z_exit, x_det, y_det, z_det)[view_cut]  #np.sqrt( x_exit**2 + (z_exit - altitude - Earth_radius)**2 ) [view_cut]
     GEOM_theta_exit = np.arccos(cos_theta_exit[view_cut])*180./np.pi
-    exit_view_angle = exit_view_angle[view_cut]
+    zenith_angle = np.pi/2. - exit_view_angle[view_cut] # radians
+    exit_view_angle = exit_view_angle[view_cut] #radians
+    # set up some arrays to be calculated on the fly
+    dist_decay_to_detector = np.zeros(len(dist_exit_to_detector))
+    x_decay = np.zeros(len(x_exit[view_cut]))
+    y_decay = np.zeros(len(y_exit[view_cut]))
+    z_decay = np.zeros(len(z_exit[view_cut]))
+    decay_view_angle = np.zeros(len(exit_view_angle))
+    X0_dist = np.zeros(len(dist_exit_to_detector))
+
     # 3. Load Energy Look-up Table
     LUT_th_exit, LUT_P_exit, LUT_log10_E_tau = load_tau_LUTs(LUT_file_name)
-
-    # 4. Loop Through Geometry Events  
+    
+    # 4. Load the Efield interpolator for this altitude
+    global efield_interpolator_list
+    efield_interpolator_list = load_efield_interpolator(altitude, EFIELD_LUT_file_name)
+    
+    # 5. Loop Through Geometry Events  
     sum_P_exit = 0.
     sum_P_exit_P_range = 0.
     sum_P_exit_P_range_P_det = 0.    
     triggered_events = []
     for k in range(0,len(GEOM_theta_exit)):
-        # 4.1 Get LUT exit angle closest to geometry exit angle
+        # 5.1 Get LUT exit angle closest to geometry exit angle
         idx = np.argmin(np.abs(LUT_th_exit - GEOM_theta_exit[k]))
 
-        # 4.2 Get tau lepton energy and decay position
+        # 5.2 Get tau lepton energy and decay position
         P_range = 0.                  # zero until it is proven to decay before it passes the detector
         P_det = 0.                    # zero until it is proven to be detectable
         if( LUT_P_exit[idx] > 1.e-15):  # make sure the probability of this event is non-zero
             log10_tau_energy = LUT_log10_E_tau[idx][np.random.randint(0,len(LUT_log10_E_tau[idx]))] # random tau energy
             decay_range = tau_lepton_decay_range(log10_tau_energy)                      # estimated decay range
-            X0_dist = np.random.exponential(scale=decay_range)                    # sample exponentially distributed decay positions            
-            if(X0_dist < dist_to_detector[k]): # If the event is contained the range probability is 1. 
+            X0_dist[k] = np.random.exponential(scale=decay_range)                          # sample exponentially distributed decay positions
+	    x_decay[k], y_decay[k], z_decay[k], decay_view_angle[k], dist_decay_to_detector[k] = decay_point_geom(k_x[k], k_y[k], k_z[k], x_exit[k], y_exit[k], z_exit[k], X0_dist[k], x_det, y_det, z_det, exit_view_angle[k])
+            if(X0_dist[k] < dist_exit_to_detector[k]): # If the event is contained within the range, then the probability is 1.
                 P_range = 1.
                 #print exit_view_angle[k]*180./np.pi
                 # TODO: calculate the exit_view_angle from the decay point rather than the exit point
-                Peak_E_field = E_field_model(exit_view_angle[k]*180./np.pi, log10_tau_energy, dist_to_detector[k])
+		# E_field_interp(view_angle_deg, zenith_angle_deg, f_Lo, f_High, log10_tau_energy, distance_km)
+                Peak_E_field = E_field_interp(decay_view_angle[k], zenith_angle[k]*180./np.pi, f_Lo, f_High, log10_tau_energy, dist_decay_to_detector[k])
                 Peak_Voltage = E_to_V_signal(Peak_E_field)
-                if(Peak_Voltage > 3.*threshold_voltage):
+		# in 10-MHz steps, to match the frequency bins of the peak voltage
+		Noise_Voltage = galactic_noise_voltage(np.arange(f_Lo, f_High+10, 10))
+		Peak_Voltage_SNR = Peak_Voltage / Noise_Voltage 
+                if(Peak_Voltage_SNR > threshold_voltage_snr):
                     #print 'Peak_Voltage %1.2e, view_angle %1.2f'%(Peak_Voltage, exit_view_angle[k]*180./np.pi)
                     P_det = 1.
-                    triggered_events.append(np.array( [ log10_tau_energy, X0_dist, dist_to_detector[k], Peak_E_field, exit_view_angle[k]*180./np.pi, LUT_P_exit[idx], GEOM_theta_exit[k] ]))
+                    triggered_events.append(np.array( [ log10_tau_energy, X0_dist, dist_decay_to_detector[k], Peak_E_field, exit_view_angle[k]*180./np.pi, LUT_P_exit[idx], GEOM_theta_exit[k] ]))
             #print '%1.1f\t%1.1f\t%1.1f\t%1.1f\t%d\t%1.1f'%(log10_tau_energy, decay_range, X0_dist, dist_to_detector[k], X0_dist < dist_to_detector[k], Peak_Voltage/threshold_voltage)
         #print LUT_E_tau[idx]
         #print '%1.2f'%tau_energy
@@ -242,6 +314,8 @@ def A_OMEGA_tau_exit(geom_file_name, LUT_file_name, cut_ang, outTag='test'):
         sum_P_exit_P_range        += LUT_P_exit[idx] * P_range 
         sum_P_exit_P_range_P_det  += LUT_P_exit[idx] * P_range * P_det
         if(k%100000 == 0 and k>0):
+	#if(k%1== 0 and k>0):
+	    print 'After %d events: '%k
             print '\t %1.3e km^2 sr'%A_Omega
             print '\t %1.3e km^2 sr'%(A_Omega*sum_P_exit / float(k))
             print '\t %1.3e km^2 sr'%(A_Omega*sum_P_exit_P_range / float(k))
@@ -249,6 +323,7 @@ def A_OMEGA_tau_exit(geom_file_name, LUT_file_name, cut_ang, outTag='test'):
             print '\t ',np.array(triggered_events).shape
             print ''
 
+    print 'After all %d events: '%len(GEOM_theta_exit)
     '\t %1.3e km^2 sr'%A_Omega
     '\t %1.3e km^2 sr'%(A_Omega*sum_P_exit/float(N_cut))
     '\t %1.3e km^2 sr'%(A_Omega*sum_P_exit_P_range/float(N_cut))
@@ -260,5 +335,6 @@ def A_OMEGA_tau_exit(geom_file_name, LUT_file_name, cut_ang, outTag='test'):
                             A_Omega_trig     = A_Omega*sum_P_exit_P_range_P_det/float(N_cut),
                             triggered_events = np.array(triggered_events))
 
+    print "Wrote ", outTag+'.npz'
     exit()
 
---
+++ 03_Detector/tau_Shower_Sim_TAU.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from pylab import *
+rcParams['figure.facecolor']='white'
+rcParams['font.size']=14
+rcParams['legend.fontsize']=14
+import matplotlib
+#matplotlib.use('Agg')
+import argparse
+
+from tau_Shower_Efield_Sim_lib import *
+import os
+
+if __name__ == "__main__":
+
+  parser=argparse.ArgumentParser(description='Script the simulates tau neutrino induced tau lepton air shower detection from antennas at altitude')
+  parser.add_argument("-G",    "--geom_file",     default = os.environ['TAU_ACC_GEOM_DIR'] + '/geom_3_km_5_deg_10M_ev.npz', help='number of events to simulate', type=str)
+  parser.add_argument("-L",    "--lut_file",      default = os.environ['TAU_ACC_LUT_DIR'] + '/2.0km_ice_lowCS_stdEL/LUT_3e+17_eV.npz',     help='number of events to simulate', type=str)
+  parser.add_argument("-c",    "--cut_ang",       default = 5., help='number of events to simulate', type=float)
+  parser.add_argument("-e",    "--efield_file",   default = os.environ['TAU_ACC_ZHAIRES_DIR'] + '/interpolator_efields_3km.npz', help="interpolator file for the ZHSAireS electric fields")
+  parser.add_argument("-l",    "--start_frequency", default = 10., help="starting frequency (MHz)")
+  parser.add_argument("-s",    "--stop_frequency", default = 100., help="stopping frequency (MHz)")
+  parser.add_argument("-out_tag", "--output_tag", default = 'test', help='tag for output files', type=str)
+
+  #KLUDGE TO GET ARGPARSE TO READ NEGATIVE VALUES
+  for i, arg in enumerate(sys.argv):
+    if (arg[0] == '-') and arg[1].isdigit(): sys.argv[i] = ' ' + arg
+
+  args=parser.parse_args()
+
+A_OMEGA_tau_exit(args.geom_file, args.lut_file, args.efield_file, args.cut_ang, args.start_frequency, args.stop_frequency, outTag = args.output_tag)
+
+
